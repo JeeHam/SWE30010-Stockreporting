f3fbbfd7d182a0d538e383345c27a492a91c0a9e
dianalim3107 <62094420+dianalim3107@users.noreply.github.com>
2021-11-03 16:07:41 +0800

    Add files via upload

---
+++ stock inventory system/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta name="description" content="Managing IT Project" />
+<meta name="keywords" content="Web,programming" />
+<link href=style.css rel=stylesheet type=text/css />
+<title>Stock Inventory System</title>
+</head>
+<body>
+<h1>Stock Inventory System</h1>
+<?php
+
+ echo"<a href=addsalesrecords.php><div id='clr'><h4>Add a sales records</h4></div></a>";
+ echo"<a href=searchsalesrecords.php><div id='clr'><h4>Display a sales records</h4></div></a>";
+ echo"<a href=salesdetect.php><div id='clr'><h4>Detect sales</h4></div></a>";
+ echo"<a href=editsalesrecord.php><div id='clr'><h4>Edit sales records</h4></div></a>";
+ echo"<a href=about.php><div id='text'><h4>About this assignment</h4></div></a><br>";
+	
+ echo "Name: Ngui Ting Fung<br>
+ Student ID: 101215342<br>
+ Email: <a href=https://outlook.office.com>101215342@students.swinburne.edu.my</a><br>";
+ echo"<p>This is a group assignment. The team have work together.</p>";
+
+?>
+</body>
+</html>
